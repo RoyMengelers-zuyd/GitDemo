0d9e73cf218d2ae0d392042ca68a7965273ebf2a
Finn Frings <2504628frings@zuyd.nl>
2026-01-08 13:45:00 +0100

    ?

--- GitDemo/Program.cs
+++ GitDemo/Program.cs
@@ -4,6 +4,11 @@
     {
         static void Main(string[] args)
         {
+<<<<<<< Updated upstream
+=======
+            hond tijn = new hond("tijn", 2, false, "tijn blaft veel");
+
+>>>>>>> Stashed changes
         }
     }
 }
