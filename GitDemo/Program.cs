9e3f869af9492c3278c57de9b58845a8f1ba3f05
Finn Frings <2504628frings@zuyd.nl>
2026-01-08 13:33:07 +0100

    tijn de hond is aangemaakt

--- GitDemo/Program.cs
+++ GitDemo/Program.cs
@@ -1,9 +1,12 @@
-﻿namespace GitDemo
+﻿using GitDemo.Models;
+
+namespace GitDemo
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            hond tijn = new hond("tijn", 2, false, "tijn blaft veel");
         }
     }
 }
