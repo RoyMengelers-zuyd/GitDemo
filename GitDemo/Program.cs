f849985e8ddea8f7d43f2e25f04aeff601f95601
FabianRoso <2502026roso@zuyd.nl>
2026-01-08 13:50:53 +0100

    Hond gewijzigd

--- GitDemo/Program.cs
+++ GitDemo/Program.cs
@@ -1,13 +1,14 @@
-﻿namespace GitDemo
+﻿using GitDemo.Models;
+
+namespace GitDemo
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-<<<<<<< Updated upstream
-=======
+
             Hond tijn = new Hond("tijn", 2, false, "tijn blaft veel");
->>>>>>> Stashed changes
+
         }
     }
 }
