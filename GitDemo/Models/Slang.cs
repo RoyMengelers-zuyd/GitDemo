69e49c0c31f2cf842962f99be6ab048493ba5a18
FabianRoso <2502026roso@zuyd.nl>
2026-01-08 13:37:48 +0100

    Slang-class toegevoegd

---
+++ GitDemo/Models/Slang.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitDemo.Models
+{
+    internal class Slang: Dier
+    {
+        public Slang(string naam) : base(naam)
+        {
+        }
+
+        public override string Bewegen()
+        {
+            return "Slang kruipt vooruit";
+        }
+    }
+}
