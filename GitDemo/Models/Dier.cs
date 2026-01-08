eb2dcd77243034990283cf530454a1ff16d75938
Roy Mengelers <roy.mengelers@zuyd.nl>
2026-01-08 13:19:50 +0100

    basisklasse Dier toegevoegd

---
+++ GitDemo/Models/Dier.cs
@@ -0,0 +1,14 @@
+﻿namespace GitDemo.Models
+{
+    internal abstract class Dier
+    {
+        public string Naam { get; private set; }
+
+        protected Dier(string naam)
+        {
+            Naam = naam;
+        }
+
+        public abstract string Bewegen();
+    }
+}
