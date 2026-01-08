27a96baeb97b06916431670c4e29991feff79739
FabianRoso <2502026roso@zuyd.nl>
2026-01-08 13:49:12 +0100

    wijziging

---
+++ GitDemo/Models/hond.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitDemo.Models
+{
+    internal class Hond : Dier
+    {
+        public int Aantalpoten { get; private set; }
+        public bool Veelgeld { get;  set; }
+
+        public string Blaffen { get;  set; } = "tijn blaft veel";
+        public Hond(string naam, int aantalpoten, bool veelgeld, string blaffen) : base(naam)
+        {
+            Aantalpoten = aantalpoten; 
+            Veelgeld = veelgeld;
+            Blaffen = blaffen;
+        }
+        public override string Bewegen()
+        {
+            if (Veelgeld == false)
+            {
+                return "kan niet bewegen want is gehandicapd";
+            }
+            else
+            {
+                return "kan bewegen";
+            }
+        }
+    }
+}
+
